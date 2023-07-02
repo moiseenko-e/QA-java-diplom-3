03c53f444ca4820a548d5468f2256522e58f6a3c
moiseenko-e <126522295+moiseenko-e@users.noreply.github.com>
2023-07-02 23:19:39 +0300

    Commit changes
    Исправлен локатор в элементе кнопка "Восстановить пароль" в классе LoginPage ;
    Вынесено значение, по которому я проверяю прохождение теста,  в константу класса LoginPageTest ;
    Создан метод для получения значения атрибута элемента таба в классе MainPage, исправлен тест bunTest в классе MainPageTests

---
+++ MainPageTests.java
@@ -0,0 +1,68 @@
+package praktikum;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import io.qameta.allure.junit4.DisplayName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import praktikum.data.BaseData;
+import praktikum.data.WebDriverConfig;
+import praktikum.pageobject.MainPage;
+
+import java.time.Duration;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainPageTests {
+    private WebDriver driver;
+     MainPage objHomePage;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.navigate().to(BaseData.BASE_URL);
+        objHomePage = new MainPage(driver);
+    }
+
+    @Test
+    @DisplayName("Таб 'Булки'")
+    public void bunTest() {
+        objHomePage.clickFillingButton();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WebDriverConfig.WAIT_SECONDS_TIMEOUT));
+        objHomePage.clickBunButton();
+        String newBreadText = objHomePage.getBunText();
+        assertThat(newBreadText, is("Булки"));
+
+        String attributeValue = objHomePage.getAttributeValueClassFromParentByElement("Булки");
+        assertThat(attributeValue, containsString("tab_tab_type_current"));
+    }
+
+    @Test
+    @DisplayName("Таб 'Соусы'")
+    public void sauceTest() {
+        objHomePage.clickSauceButton();
+        String newSauceText = objHomePage.getSauceText();
+        assertThat(newSauceText, is("Соусы"));
+    }
+
+    @Test
+    @DisplayName("Таб 'Начинки'")
+    public void fillingTest() {
+        objHomePage.clickFillingButton();
+        String newFillingText = objHomePage.getFillingText();
+        assertThat(newFillingText, is("Начинки"));
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
